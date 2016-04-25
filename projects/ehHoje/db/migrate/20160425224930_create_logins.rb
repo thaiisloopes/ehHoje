0190a3261616bd762af15079d99e7224a0bf9923
Thais Lopes Nunes <tlnloopes@gmail.com>
2016-04-25 19:57:50 -0300

    Inicio do Projeto

---
+++ projects/ehHoje/db/migrate/20160425224930_create_logins.rb
@@ -0,0 +1,11 @@
+class CreateLogins < ActiveRecord::Migration
+  def change
+    create_table :logins do |t|
+      t.string :name
+      t.text :description
+      t.string :picture
+
+      t.timestamps null: false
+    end
+  end
+end
