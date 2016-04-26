04fd636ccaca970badeafffbf34869bca857ea75
Thais Lopes Nunes <tlnloopes@gmail.com>
2016-04-25 22:11:37 -0300

    Criando Paginas padrão

--- projects/ehHoje/config/routes.rb
+++ projects/ehHoje/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
   resources :logins
   resources :ideas
   # The priority is based upon order of creation: first created -> highest priority.
